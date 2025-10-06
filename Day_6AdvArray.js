42898503f1e4a070c86ae314773243d1441967a8
Charvi Khandar <charvikhandar2@gmail.com>
2025-10-06 22:20:19 +0530

    Add files via upload

--- Day_6AdvArray.js
+++ Day_6AdvArray.js
@@ -109,3 +109,19 @@ while(i<arr1.length){
 }
 
 console.log(merge)
+
+//Find the maximum profit from the given set of prices of stock 
+//Best time to buy and sell stock
+
+let maxProfit = function(prices){
+    let maxProfit = 0;
+    let min = prices[0]
+    for(let i=0; i <prices.length;i++){
+        if(prices[i]<min) min = prices[i]
+       let profit = prices[i] - min;
+        maxProfit = Math.max(profit,maxProfit)
+    }
+    return maxProfit
+}
+
+console.log(maxProfit([7,1,5,3,6,4]))
