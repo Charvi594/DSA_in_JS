96cd576b924c7ab0e0c77d7202c039b88ed1db05
Charvi Khandar <charvikhandar2@gmail.com>
2025-10-07 23:55:56 +0530

    Add files via upload
    implemented maximum subarray problem with kadane's algorithm

---
+++ Day_7MoreArray.js
@@ -0,0 +1,14 @@
+//Maximum sum from subarray
+
+let nums = [-2,1,-3,4,-1,2,1,-5,4]
+let sum = 0;
+let maxSum = -Infinity
+
+for(let i=0;i<nums.length;i++){
+    sum+=nums[i]
+     if(sum < 0) sum =0
+     maxSum = Math.max(sum,maxSum)
+   
+   
+}
+console.log(maxSum);
