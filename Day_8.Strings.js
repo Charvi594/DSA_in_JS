3d6fb429b62238543e3f4959954eb148898604a1
Charvi Khandar <charvikhandar2@gmail.com>
2025-10-18 10:19:37 +0530

    Add files via upload
    implemented palindrome of a string

--- Day_8.Strings.js
+++ Day_8.Strings.js
@@ -1,7 +1,23 @@
+const prompt = require("prompt-sync")()
 //Reversal of a string
-let str = "Charvi Khandar";
-let rev = " ";
-for(let i=str.length-1;i>=0;i--){
-    rev+=str.charAt(i)
+// let str = "Charvi Khandar";
+// let rev = " ";
+// for(let i=str.length-1;i>=0;i--){
+//     rev+=str.charAt(i)
+// }
+// console.log(rev)
+
+//Palindrome or not
+//First find the reverse of a string
+//If the original string and the reverse of a string is equal/same, then the given string is palindrome
+
+let s = prompt("Enter a string to check: ")
+let reverse = ""
+for(let i = s.length-1;i>=0;i--){
+    reverse+=s.charAt(i)
+    
 }
-console.log(rev)
+console.log( reverse)
+if(reverse===s) console.log("The string is palindrome.")
+else {console.log( "The string is not a palindrome.")}
+
