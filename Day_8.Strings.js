da768906454534bfe86e420b43d2974cebdb23b7
Charvi Khandar <charvikhandar2@gmail.com>
2025-10-18 10:34:42 +0530

    Add files via upload
    implemented optimized version of palindrome

--- Day_8.Strings.js
+++ Day_8.Strings.js
@@ -11,13 +11,30 @@ const prompt = require("prompt-sync")()
 //First find the reverse of a string
 //If the original string and the reverse of a string is equal/same, then the given string is palindrome
 
-let s = prompt("Enter a string to check: ")
-let reverse = ""
-for(let i = s.length-1;i>=0;i--){
-    reverse+=s.charAt(i)
+// let s = prompt("Enter a string to check: ")
+// let reverse = ""
+// for(let i = s.length-1;i>=0;i--){
+//     reverse+=s.charAt(i)
+    
+// }
+// console.log( reverse)
+// if(reverse===s) console.log("The string is palindrome.")
+// else {console.log( "The string is not a palindrome.")}
+
+//Optimised version of checking palindrome of a string
+
+let s = "abba"
+let i=0
+let j = s.length-1
+let ispalindrome = true
+while(i<j){
+    if(s.charAt(i)!==s.charAt(j)){
+        ispalindrome = false;
+        break;
+    }
+    i++,j--;
     
 }
-console.log( reverse)
-if(reverse===s) console.log("The string is palindrome.")
-else {console.log( "The string is not a palindrome.")}
+if(ispalindrome) console.log("Palindromic string.")
+    else console.log("Not a palindromic string.")
 
