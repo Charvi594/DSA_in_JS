17f6b4218888f34bc3de095de282fd05e52fdac9
Charvi Khandar <charvikhandar2@gmail.com>
2025-10-19 11:05:32 +0530

    Add files via upload
    implemented toggle character and frequency of a character program

--- Day_8.Strings.js
+++ Day_8.Strings.js
@@ -38,3 +38,30 @@ while(i<j){
 if(ispalindrome) console.log("Palindromic string.")
     else console.log("Not a palindromic string.")
 
+//Toggle character
+
+let inp  = prompt("Enter a string to be toggled : ");
+let toggle = ""
+for(let i=0; i<=inp.length;i++){
+    let ch = inp.charCodeAt(i);
+
+    if(ch>=65 && ch<=90) toggle += String.fromCharCode(ch+32)
+        else if(ch>=97 && ch<=122) toggle += String.fromCharCode(ch-32)
+}
+console.log(inp);
+console.log(toggle);
+
+//Frequency of a character
+
+let str = prompt("Enter a string to calculate its frequency : ")
+let obj = {}
+for(let s of str){
+    if(obj[s]){
+        obj[s]+=1
+    }
+    else{
+        obj[s] = 1
+    }
+}
+console.log(str)
+console.log(obj)
