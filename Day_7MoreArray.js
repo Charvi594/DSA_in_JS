0e4fc0e0e9b3f5e48ce93d1ae8084b8877cc68c9
Charvi Khandar <charvikhandar2@gmail.com>
2025-10-22 08:03:19 +0530

    Add files via upload
    Implmented kadane's algorithm of returning maxSum along with a subArray

--- Day_7MoreArray.js
+++ Day_7MoreArray.js
@@ -25,25 +25,48 @@
 // }
 // console.log(ans)
 
-//Trapping the water
-var trap = function(height){
-    let left = new Array(height.length)
-    let right = new Array(height.length)
-    let maxLeft = height[0],  maxRight = height[height.length-1];
-    left[0] = maxLeft , right[right.length-1] = maxRight
-    for(let i = 1; i<height.length;i++){
-        maxLeft = Math.max(height[i],maxLeft)
-        left[i] = maxLeft
+// //Trapping the water
+// var trap = function(height){ 
+//     let left = new Array(height.length)
+//     let right = new Array(height.length)
+//     let maxLeft = height[0],  maxRight = height[height.length-1];
+//     left[0] = maxLeft , right[right.length-1] = maxRight
+//     for(let i = 1; i<height.length;i++){
+//         maxLeft = Math.max(height[i],maxLeft)
+//         left[i] = maxLeft
+//     }
+//     for(let i = height.length-2; i >=0;i--){
+//         maxRight = Math.max(height[i],maxRight)
+//         right[i] = maxRight
+//     }
+//     let ans = 0;
+//     for(let i = 0;i<height.length;i++){
+//         ans+= Math.min(left[i],right[i])-height[i]
+//     }
+//     return ans;
+// }
+
+// console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+
+//Return a max subarray along with the maxsum
+
+var maxArray = function(nums){
+let sum = 0;
+let maxSum = -Infinity
+let tempStart , start , end = 0
+for(let i=0;i<nums.length;i++){
+    if(sum<0) {sum =0 
+        tempStart = i;}
+    sum+=nums[i]
+    if(sum>maxSum){
+        maxSum = sum
+        start = tempStart
+        end = i
     }
-    for(let i = height.length-2; i >=0;i--){
-        maxRight = Math.max(height[i],maxRight)
-        right[i] = maxRight
-    }
-    let ans = 0;
-    for(let i = 0;i<height.length;i++){
-        ans+= Math.min(left[i],right[i])-height[i]
-    }
-    return ans;
+}
+console.log(nums.slice(start,end+1),maxSum);
+
 }
 
-console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+maxArray([-2,1,-3,4,-1,2,1,-5,4])
+
