fc67e7f741a136cdd471a0c0cdec985b46e57b91
Charvi Khandar <charvikhandar2@gmail.com>
2025-10-26 13:58:59 +0530

    Add files via upload
    Implemented the leaders in an array question

--- Day11.js
+++ Day11.js
@@ -18,3 +18,22 @@ var findMax = function(nums){
 }
 console.log(findMax([1,1,0,1,1,1]))
 console.log(findMax([1,0,1,1,0,1]))
+
+
+//Leaders in an Array
+//The question says that if the current element is greater than the rest of the elements to its right than it is the leader el.
+//Intuition:
+//It is observed that the last element will always be a leader, so we will first push it into the leaders array and then we will reverse traverse from it and will compare if the current element is greater than or equal to the last element. If the condition satisfies that particular element will be pushed into the array and also it will be the next leader element for comparison.
+
+let arr=[16,17,4,3,5,2];
+let leaders=[];
+let leaderEl = arr[arr.length-1];
+leaders.push(leaderEl);
+for(let i = arr.length-2;i>=0;i--){
+    if(arr[i]>=leaderEl){
+        leaders.push(arr[i]);
+        leaderEl = arr[i];
+    }
+    
+}
+console.log(leaders.reverse());
