3eb05c2639dbc7085aca6368059d465c28592f07
Charvi Khandar <charvikhandar2@gmail.com>
2025-11-09 10:10:07 +0530

    Add files via upload
    Practiced basic recursion problems

--- Day21.js
+++ Day21.js
@@ -26,3 +26,39 @@ while(left<right){
 return res;
 }
 console.log(threeSum([-1,0,1,2,-1,-4]))
+
+//Recursion Practice
+//Print numbers from 1 to n and n to 1.
+function num(n,i){
+    if(i>n) return;
+    console.log(i);
+    num(n,i+1);
+}
+num(10,1)
+
+//Print numbers from n to 1
+
+function count(i,n){
+    if(n<i) return;
+console.log(n);
+count(i,n-1);
+}
+count(1,10)
+
+//Print 1 to n using backtracking.
+console.log("Numbers from 1 to n using backtracking : ");
+
+function back(n, i){
+    if(n<i) return;
+back(n-1,i);
+console.log(n);
+}
+back(10,1)
+//Print n to 1 using backtracking.
+console.log("10 to 1 using backtracking : ");
+function backs(n,i){
+    if(i>n) return;
+backs(n,i+1);
+console.log(i);
+}
+backs(10,1)
