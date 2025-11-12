631e4b747f492975ba69ea97e63a71d4505bf4d9
Charvi Khandar <charvikhandar2@gmail.com>
2025-11-12 21:40:09 +0530

    Add files via upload
    Implemented hcf problem

--- Day23.js
+++ Day23.js
@@ -5,9 +5,21 @@ function rev(num){
 
     let reverse = 0;
     while(num!==0){
-         reverse = reverse * 10 + (num%10);
+        reverse = reverse * 10 + (num%10);
         num = Math.floor(num/10);
     }
     return reverse;
 }
 console.log(rev(13112005));
+
+//Find the greatest Common divisor 
+function findhcf(num1, num2){
+    let hcf = 0;
+    for(let i=1 ; i<=num1 && i<= num2 ; i++){
+        if(num1%i==0 && num2%i == 0){
+            hcf  = i;
+        }
+    }
+    return hcf;
+}
+console.log(findhcf(10,5))
